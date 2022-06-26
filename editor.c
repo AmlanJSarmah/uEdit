a558197a3fcec8dc59cdf9ed166d43a19b86112d
A-J-Sarmah <ajsarmah@tutanota.com>
2022-06-26 15:49:24 +0530

    Display keypresses

--- editor.c
+++ editor.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <termios.h>
 #include <terminal.h>
+#include <ctype.h>
 
 struct termios original_terminal_config;
 
@@ -9,7 +10,17 @@ int main()
 {
   char c;
   enable_raw_mode(&original_terminal_config); //comes from terminal.h it enables the raw mode in terminal
-  while(read(STDIN_FILENO,&c,1) == 1 && c != 'q'); // we use read() so that we don't generate any input buffer.
+  while(read(STDIN_FILENO,&c,1) == 1 && c != 'q') // we use read() so that we don't generate any input buffer.
+  {
+    if (iscntrl(c))
+    {
+      printf("%d\n", c);
+    } 
+    else 
+    {
+      printf("%d ('%c')\n", c, c);
+    }
+  }
   disable_raw_mode(&original_terminal_config); //comes from terminal.h it disables the raw mode upon exit
   return 0;
 }
