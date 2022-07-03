dfaae62b3fc8da21392c9da5e9894fc9496d0cae
A-J-Sarmah <ajsarmah@tutanota.com>
2022-07-03 13:24:33 +0530

    Put the cursor at start after clearing

--- utils.c
+++ utils.c
@@ -5,6 +5,9 @@
 
 // Note on Escape sequence character:
 // It always begin with \x1b which is equivalent to 27. It instruct the terminal to do various text formatting tasks, such as coloring text, moving the cursor around, and clearing parts of the screen.
+//It is always followed by a [
+
+// Understanding "\x1b[2J"
 // J is erase in display command to erase parts of screen it is precceded by a 2 which is the argument for J.
 // 2 means clearing entire screen while 1 clear screen up to the cursor from the start and 0 will clear screen from cursor to the end. 0 is the default argument.
 
@@ -19,6 +22,10 @@ void clear_screen()
 {
     // we are using 4 in write as we are writing 4 bytes in the terminal
     //The first byte is \x1b, which is the escape character, or 27 in decimal.
-    //other three bytes are [2j
+    //other three bytes are [2j . It clears screen
     write(STDOUT_FILENO, "\x1b[2J", 4);
+    // we are using 3 in write as we are writing 4 bytes in the terminal
+    //The first byte is \x1b, which is the escape character, or 27 in decimal.
+    //other two bytes are [H. It reposition cursor
+    write(STDOUT_FILENO,"\x1b[H",3);
 }
