ae27e47395a93999ad96cf41fca95b12672dd984
A-J-Sarmah <ajsarmah@tutanota.com>
2022-08-13 21:29:39 +0530

    Provided ifdef to remove collision errors

--- headers/terminal.h
+++ headers/terminal.h
@@ -1,4 +1,7 @@
-#include <termios.h>
+#ifndef _TERMINAL__H__
+#define _TERMINAL__H__
 
 void enable_raw_mode(struct termios *original_terminal_config);
 void disable_raw_mode(struct termios *original_terminal_config);
+
+#endif
--- headers/utils.h
+++ headers/utils.h
@@ -1,6 +1,9 @@
-#include <editor.h>
+#ifndef _UTILS__H__
+#define _UTILS__H__
 
 void emergency_exit(const char *error);
 void clear_screen();
 void draw_rows(int no_of_rows, int no_of_cols);
 int get_window_size(int *no_of_rows, int *no_of_columns);
+
+#endif
